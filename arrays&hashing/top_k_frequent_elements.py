d074cc042dae774c153d355de8a0010b6966048a
europeangeek <mczajadeveloper@gmail.com>
2023-10-07 23:12:54 +0100

    solution with Counter object

---
+++ arrays&hashing/top_k_frequent_elements.py
@@ -0,0 +1,36 @@
+# Given an integer array nums and an integer k, 
+# return the k most frequent elements. You may return the answer in any order.
+from collections import Counter
+# Example 1:
+
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+ 
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+# k is in the range [1, the number of unique elements in the array].
+# It is guaranteed that the answer is unique.
+ 
+# Follow up: Your algorithm's time complexity must be better than O(n log n),
+# where n is the array's size.
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counter_dict = Counter(nums)
+        print(counter_dict)
+        return [k for k,v in Counter(nums).most_common(k)]
+nums = [1,1,1,2,2,3,7,8,9,9,9] 
+k = 2
+solution = Solution().topKFrequent(nums,k)
+print(solution)
