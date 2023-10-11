c8a39a32f0cd9fe71ecf2fbb93ee20f22a4eb716
europeangeek <mczajadeveloper@gmail.com>
2023-10-11 01:21:12 +0100

    my solution of palindrom without extra memory

--- twoPointers/palindrome.py
+++ twoPointers/palindrome.py
@@ -36,13 +36,38 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        valid_letters = string.ascii_letters + string.digits
-        print(valid_letters)
-        cleared_s = "".join([letter for letter in s if letter in valid_letters]).lower()
-        reversed_s = cleared_s[::-1]
-        return cleared_s == reversed_s
+        # this is first basic solution that came up to my mind. Its linear but with use of additional memory
+        # valid_letters = string.ascii_lowercase + string.digits
+        # print(valid_letters)
+        # cleared_s = "".join([letter for letter in s if letter in valid_letters]).lower()
+        # reversed_s = cleared_s[::-1]
+        # return cleared_s == reversed_s
+        # solution with a pointers
+        valid_letters = string.ascii_lowercase + string.digits
+        l = 0
+        r = len(s) -1
+        while l != r:
+            left_letter = s[l].lower()
+            right_letter = s[r].lower()
+            if left_letter not in valid_letters:
+                l += 1
+                continue
+            if right_letter not in valid_letters:
+                r -= 1
+                continue
+            if left_letter == right_letter:
+                if r - l > 1:
+                    l += 1
+                    r -= 1
+                    continue
+                return True
+            else:
+                return False
+        return True
+                    
 
-palindrome = "A man, a plan, a canal: Panama"
+
+palindrome = "abba"
 # palindrome = "0P"
 solution = Solution()
 print(solution.isPalindrome(palindrome))
