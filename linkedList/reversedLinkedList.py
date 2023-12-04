8908df4dd14cdcd608e0bdd836257de3532be928
europeangeek <mczajadeveloper@gmail.com>
2023-12-04 22:53:36 +0000

    iterative O(n) time complexity and O(1) memory complexity

---
+++ linkedList/reversedLinkedList.py
@@ -0,0 +1,11 @@
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+example
+
+# original linkeList
+# 1 -> 2 -> 3 -> 4 -> 5
+
+# reversed linkedList
+# 5 -> 4 -> 3 -> 2 -> 1
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
