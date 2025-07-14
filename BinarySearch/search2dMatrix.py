ea3937dd5c5e663b1a797c6df2f4c03ab7875e3f
europeangeek <mczajadeveloper@gmail.com>
2025-07-15 00:17:58 +0100

    feat:searchMatrix improved algorhitm to o(log m + n)

--- BinarySearch/search2dMatrix.py
+++ BinarySearch/search2dMatrix.py
@@ -2,30 +2,34 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # TODO: There is a list of matrix which is sorted, algo that must be used here is called binary search
-        # TODO: Current solution is o(n), find a solution log (m * n) 
-        row = 1.5
-        for i in range(len(matrix)):
-            if target >= matrix[i][0] and target <= matrix[i][-1]:
-                row = i
-        print(row)
-        if row == 1.5:
-            return False
-            
-        left, right = 0, len(matrix[row]) - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if matrix[row][mid] == target:
-                return True
-            elif matrix[row][mid] > target:
-                right = mid - 1
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                bot = row + 1
+            elif target < matrix[row][0]:
+                top = row - 1
             else:
-                left = mid + 1
+                break
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l+r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
         return False
+        
+                
+            
+            
 
-
-
-
-matrix, target = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], 3
+matrix, target = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], 11
 s = Solution()
 print(s.searchMatrix(matrix, target))
