7832f4bc9e2390f5c65cb1de7742942d29f82201
europeangeek <mczajadeveloper@gmail.com>
2025-07-14 01:47:55 +0100

    feat(twoPointers): Enhance 3sum solution with type hints and comments
    - Add type hints to the threeSum function for improved readability and static analysis.
    - Incorporate detailed comments to clarify the two-pointer algorithm, including edge case handling for duplicates and sorted input.

--- twoPointers/3sum.py
+++ twoPointers/3sum.py
@@ -30,32 +30,43 @@
 
 # 3 <= nums.length <= 3000
 # -105 <= nums[i] <= 105
-
+from typing import List
 class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
-
-        for i,a in enumerate(nums):
+        
+        for i, a in enumerate(nums):
+            # our aim is to return a list of numbers that adds up to 0, so if the first element is already bigger than 0 and the list is sorted its impossible to return a sum of three numbers which will be equal to zero
+            if a > 0:
+                break
+            # we dont want to cause compare  -indexes so we skip 0
+            # and because the list is sorted we can have same values next to each other, there we use that fact to skip same values because we dont want duplicates in our solution
             if i > 0 and a == nums[i - 1]:
                 continue
-            l, r = i + 1, len(nums) -1
+            
+            # we will always operate on the piece of array after i, because we want our iterations number as small as possible.
+            l, r = i + 1, len(nums) - 1
+            # we want to iterate as long as l != r
             while l < r:
                 threeSum = a + nums[l] + nums[r]
+                # the list is already sorted so if the threesum is bigger than 0 we need to decrease r pointer to the left to have smaller total value
                 if threeSum > 0:
                     r -= 1
+                # opposite operation, we want our threesum to be bigger therefore we gonna increment l point by 1
                 elif threeSum < 0:
                     l += 1
+                # if we go into else this means that we have matched 0 and we will append the result
                 else:
                     res.append([a, nums[l], nums[r]])
+                    # i dont get it from that point, tell me why we increase l pointer, not decrease r pointer
                     l += 1
-                    while nums[l] == nums[l +1] and l < r:
+                    # so here we want to switch to another unique element
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
         return res
-nums = [-1,0,1,2,-1,-4]
+                
+
+nums = [0,2,0,3,1,0,1,3,2,1]
 solution = Solution()
-print(solution.threeSum(nums))
+print(solution.trap(nums))
