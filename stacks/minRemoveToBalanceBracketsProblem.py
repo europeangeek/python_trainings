dd3ecea45b31da8ef97737b1a637fa71c248594e
europeangeek <mczajadeveloper@gmail.com>
2025-07-17 02:35:15 +0100

    feat: Updated version to include any type of opening or closing paranthesis

--- stacks/minRemoveToBalanceBracketsProblem.py
+++ stacks/minRemoveToBalanceBracketsProblem.py
@@ -1,18 +1,25 @@
 from typing import List
-# Create a function that determines the minimum number of bracket removals needed for a valid string.
-def min_removals_to_balance(brackets: str) -> int:
+# Create a function that determines the minimum number of c removals needed for a valid string.
+def min_removals_to_balance(s: str) -> int:
     stack: List[str] = []
     count: int = 0
-    
-    for bracket in brackets:
-        if bracket == '(':
-            stack.append(bracket)
-        elif bracket == ')':
-            if stack and stack[-1] == '(':
+    closeToOpen = {
+            "]": "[",
+            "}": "{",
+            ")": "("
+    }
+    for c in s:
+        if c in closeToOpen.values():
+            stack.append(c)
+        elif c in closeToOpen:
+            if stack and stack[-1] == closeToOpen[c]:
                 stack.pop()
             else:
                 count += 1
+                print(c)
 
     return count + len(stack)
 
 #TODO: Create a function that determines the minimum number of any paranthesis removals needed for a valid string.
+
+print(min_removals_to_balance("({{})))(())")) 
